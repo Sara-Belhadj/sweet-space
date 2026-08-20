84474e4befb83fbacb9ca213acf81f26ee306c02
Sara-Belhadj <sara.belhadj.dev@gmail.com>
2026-08-20 14:23:00 -0400

    Improve navigation UI by adding text icons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+/* START BUTTON */
 const startButton = document.querySelector(".startButton");
 
+if(startButton) {
     startButton.addEventListener("click", function() {
         window.location.href = "homePage.html";
     });
+}
+
+/* HOME PAGE NAVIGATION */
+const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function() {
+
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+            });
+
+        this.classList.add("active");
+    });
+
+});
